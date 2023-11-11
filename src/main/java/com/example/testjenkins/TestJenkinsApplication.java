db75cc7280f84993a3a3b393fba4afbb4ac1f491
abdessalembendhia <abdessalembendhia.it@gmail.com>
2023-11-11 20:56:45 +0300

    tet

--- src/main/java/com/example/testjenkins/TestJenkinsApplication.java
+++ src/main/java/com/example/testjenkins/TestJenkinsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestJenkinsApplication {
-
+/////////////
     public static void main(String[] args) {
         SpringApplication.run(TestJenkinsApplication.class, args);
     }
